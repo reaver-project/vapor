4a8b2e7c630e22b25ea524c89bd1fb140142603a
Michał 'Griwes' Dominiak <griwes@griwes.info>
2018-05-14 05:57:37 +0200

    codegen: make linker errors more sensible by actually emitting symbols.

--- lib/codegen/llvm_ir/function.cpp
+++ lib/codegen/llvm_ir/function.cpp
@@ -52,7 +52,7 @@ inline namespace _v1
         }
 
         ret += fn.is_defined ? U"define " : U"declare ";
-        ret += fn.is_builtin ? U"linkonce_odr " : (!fn.is_defined || fn.is_exported ? U"" : U"private ");
+        ret += fn.is_builtin ? U"linkonce_odr " : (!fn.is_defined || fn.is_exported ? U"" : U"internal ");
         ret += type_name(ir::get_type(fn.return_value), ctx);
         ret += U" @\"" + scopes + function_name(fn, ctx);
         ret += U"\"(\n";
